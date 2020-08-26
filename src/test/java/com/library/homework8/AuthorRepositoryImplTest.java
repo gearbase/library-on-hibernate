24ae99daba6fbc736476579a61aa3c7915166278
gearbase <bsa8731@gmail.com>
2020-08-26 17:37:41 +0300

    Добавление библиотеки Log4jdbc-log4j2 для отображения SQL запросов в хибере
    https://github.com/candrews/log4jdbc-spring-boot-starter
    http://log4jdbc.brunorozendo.com/
    https://mvnrepository.com/artifact/com.integralblue/log4jdbc-spring-boot-starter/2.0.0

--- src/test/java/com/library/homework8/AuthorRepositoryImplTest.java
+++ src/test/java/com/library/homework8/AuthorRepositoryImplTest.java
@@ -2,11 +2,16 @@ package com.library.homework8;
 
 import com.library.homework8.dao.AuthorRepository;
 import com.library.homework8.domain.Author;
+import com.library.homework8.domain.Genre;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -15,10 +20,14 @@ public class AuthorRepositoryImplTest {
     AuthorRepository authorRepository;
 
     @Test
+    @Transactional
     public void test() {
         Author author = authorRepository.getById(1L);
         Author author1 = authorRepository.findById(2L).orElseThrow();
         Author author2 = authorRepository.findAuthor(1L);
-        System.out.println(author.getName());
+        List<String> genres1 = author.getGenres().stream().map(Genre::getName).collect(Collectors.toList());
+        List<String> genres2 = author2.getGenres().stream().map(Genre::getName).collect(Collectors.toList());
+        System.out.println(genres1);
+        System.out.println(genres2);
     }
 }
